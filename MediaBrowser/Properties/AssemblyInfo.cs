13c0ef1fa57786bfbd678a399ab5c335f4246a9c
ScottIsAFool <unknown@kiln.example.com>
2013-02-04 22:05:37 +0000

    Updated NeutralResourcesLanguageAttribute for wp7 beta submission.

--- MediaBrowser/Properties/AssemblyInfo.cs
+++ MediaBrowser/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
