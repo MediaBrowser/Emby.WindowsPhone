70664ad5c8292364e5521e05ff8a80ff918ef58f
ScottIsAFool <scottisafool@live.co.uk>
2013-07-17 17:10:42 -0100

    Added NotificationLevel converter

---
+++ MediaBrowser.WindowsPhone7/Converters/NotificationLevelToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using MediaBrowser.Model.Notifications;
+
+namespace MediaBrowser.WindowsPhone.Converters
+{
+    public class NotificationLevelToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var notificationLevel = (NotificationLevel) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
