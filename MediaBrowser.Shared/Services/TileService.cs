dc48806a2182ce80027e78f8d4fd261c5bc2f456
ScottIsAFool <scottisafool@live.co.uk>
2013-07-30 05:28:53 -0100

    Added a TileService Service

---
+++ MediaBrowser.Shared/Services/TileService.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Cimbalino.Phone.Toolkit.Services;
+
+namespace MediaBrowser.Services
+{
+    public class TileService
+    {
+        private static TileService _current;
+        private static readonly ShellTileService ShellTileService = new ShellTileService();
+
+        public static TileService Current
+        {
+            get { return _current ?? (_current = new TileService()); }
+        }
+
+        public bool TileExists(string uri)
+        {
+            var tile = ShellTileService.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains(uri));
+            return tile != default(ShellTileServiceTile);
+        }
+    }
+}
