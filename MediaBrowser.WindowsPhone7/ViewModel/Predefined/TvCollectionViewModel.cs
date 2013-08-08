dfa89b0cf8c5ccbd8d03f611e11e46619e7fad0b
ScottIsAFool <scottisafool@live.co.uk>
2013-08-08 06:02:57 -0100

    Added next up stuff for TV View.

--- MediaBrowser.WindowsPhone7/ViewModel/Predefined/TvCollectionViewModel.cs
+++ MediaBrowser.WindowsPhone7/ViewModel/Predefined/TvCollectionViewModel.cs
@@ -1,4 +1,14 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using MediaBrowser.Model;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Model.Net;
+using MediaBrowser.Model.Querying;
+using MediaBrowser.Services;
+using MediaBrowser.WindowsPhone.Model;
+using ScottIsAFool.WindowsPhone.ViewModel;
 
 namespace MediaBrowser.WindowsPhone.ViewModel.Predefined
 {
@@ -10,11 +20,77 @@ namespace MediaBrowser.WindowsPhone.ViewModel.Predefined
     /// </summary>
     public class TvCollectionViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+        private readonly IExtendedApiClient _apiClient;
+
+        private bool _nextUpLoaded;
+
         /// <summary>
         /// Initializes a new instance of the TvCollectionViewModel class.
         /// </summary>
-        public TvCollectionViewModel()
+        public TvCollectionViewModel(INavigationService navigationService, IExtendedApiClient apiClient)
         {
+            _navigationService = navigationService;
+            _apiClient = apiClient;
+        }
+
+        public List<BaseItemDto> NextUpList { get; set; }
+
+        public int PivotSelectedIndex { get; set; }
+
+        public RelayCommand PageLoadedCommand
+        {
+            get
+            {
+                return new RelayCommand(async () =>
+                {
+                    if (!_navigationService.IsNetworkAvailable)
+                    {
+                        return;
+                    }
+
+
+                });
+            }
+        }
+
+        private async Task<bool> GetNextUp()
+        {
+            try
+            {
+                SetProgressBar("Getting next up items...");
+
+                var query = new NextUpQuery
+                {
+                    UserId = AuthenticationService.Current.LoggedInUser.Id
+                };
+
+                Log.Info("Getting next up items");
+
+                var itemResponse = await _apiClient.GetNextUpAsync(query);
+
+                SetItems(itemResponse, NextUpList);
+
+                return true;
+            }
+            catch (HttpException ex)
+            {
+                Log.ErrorException("GetNextUp()", ex);
+            }
+
+            SetProgressBar();
+
+            return false;
+        }
+
+        private void SetItems(ItemsResult itemResponse, List<BaseItemDto> listToSet)
+        {
+            if (itemResponse == null || !itemResponse.Items.Any())
+            {
+                return;
+            }
+
+            listToSet = itemResponse.Items.ToList();
         }
     }
 }
