64ae63f0964cea5d9da402d7cafe0414ccbea951
ScottIsAFool <unknown@kiln.example.com>
2013-09-15 21:06:12 -0100

    Fixed websocket not working.

--- MediaBrowser.Shared/Model/WebSocketClient.cs
+++ MediaBrowser.Shared/Model/WebSocketClient.cs
@@ -3,19 +3,24 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MediaBrowser.ApiInteraction.WebSocket;
+using MediaBrowser.Model.Net;
 using WebSocket4Net;
+using WebSocketState = WebSocket4Net.WebSocketState;
 
 namespace MediaBrowser.Model
 {
     public class WebSocketClient : IClientWebSocket
     {
-        private WebSocket _socket;
+        /// <summary>
+        /// The _socket
+        /// </summary>
+        private WebSocket4Net.WebSocket _socket;
 
         /// <summary>
         /// Gets or sets the state.
         /// </summary>
         /// <value>The state.</value>
-        public Net.WebSocketState State
+        public Model.Net.WebSocketState State
         {
             get
             {
@@ -23,17 +28,17 @@ namespace MediaBrowser.Model
                 switch (_socket.State)
                 {
                     case WebSocketState.Closed:
-                        return Net.WebSocketState.Closed;
+                        return Model.Net.WebSocketState.Closed;
                     case WebSocketState.Closing:
-                        return Net.WebSocketState.Closed;
+                        return Model.Net.WebSocketState.Closed;
                     case WebSocketState.Connecting:
-                        return Net.WebSocketState.Connecting;
+                        return Model.Net.WebSocketState.Connecting;
                     case WebSocketState.None:
-                        return Net.WebSocketState.None;
+                        return Model.Net.WebSocketState.None;
                     case WebSocketState.Open:
-                        return Net.WebSocketState.Open;
+                        return Model.Net.WebSocketState.Open;
                     default:
-                        return Net.WebSocketState.None;
+                        return Model.Net.WebSocketState.None;
                 }
             }
         }
@@ -50,7 +55,7 @@ namespace MediaBrowser.Model
 
             try
             {
-                _socket = new WebSocket(url);
+                _socket = new WebSocket4Net.WebSocket(url);
 
                 _socket.MessageReceived += websocket_MessageReceived;
 
@@ -69,7 +74,12 @@ namespace MediaBrowser.Model
             return taskCompletionSource.Task;
         }
 
-        void _socket_Closed(object sender, EventArgs e)
+        /// <summary>
+        /// Handles the Closed event of the _socket control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void _socket_Closed(object sender, EventArgs e)
         {
             if (Closed != null)
             {
@@ -81,12 +91,12 @@ namespace MediaBrowser.Model
         /// Handles the MessageReceived event of the websocket control.
         /// </summary>
         /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="MessageReceivedEventArgs"/> instance containing the event data.</param>
-        void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        /// <param name="e">The <see cref="MessageReceivedEventArgs" /> instance containing the event data.</param>
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
         {
-            if (OnReceiveDelegate != null)
+            if (OnReceive != null)
             {
-                OnReceiveDelegate(Encoding.UTF8.GetBytes(e.Message));
+                OnReceive(e.Message);
             }
         }
 
@@ -94,7 +104,13 @@ namespace MediaBrowser.Model
         /// Gets or sets the receive action.
         /// </summary>
         /// <value>The receive action.</value>
-        public Action<byte[]> OnReceiveDelegate { get; set; }
+        public Action<byte[]> OnReceiveBytes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the on receive.
+        /// </summary>
+        /// <value>The on receive.</value>
+        public Action<string> OnReceive { get; set; }
 
         /// <summary>
         /// Sends the async.
@@ -104,9 +120,9 @@ namespace MediaBrowser.Model
         /// <param name="endOfMessage">if set to <c>true</c> [end of message].</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>Task.</returns>
-        public Task SendAsync(byte[] bytes, Net.WebSocketMessageType type, bool endOfMessage, CancellationToken cancellationToken)
+        public Task SendAsync(byte[] bytes, Model.Net.WebSocketMessageType type, bool endOfMessage, CancellationToken cancellationToken)
         {
-            return Task.Factory.StartNew(() => _socket.Send(bytes, 0, bytes.Length), new CancellationToken());
+            return Task.Factory.StartNew(() => _socket.Send(bytes, 0, bytes.Length), cancellationToken);
         }
 
         /// <summary>
@@ -121,11 +137,11 @@ namespace MediaBrowser.Model
             }
         }
 
+        /// <summary>
+        /// Occurs when [closed].
+        /// </summary>
         public event EventHandler Closed;
 
 
-        public Action<string> OnReceive { get; set; }
-
-        public Action<byte[]> OnReceiveBytes { get; set; }
     }
 }
