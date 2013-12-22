814acad3071a632d556971f7d3d946fde5ffd737
ScottIsAFool <scottisafool@live.co.uk>
2013-12-22 14:30:44 +0000

    Fixes issue #60

---
+++ MediaBrowser.WindowsPhone8/Converters/EpisodeNumberConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using MediaBrowser.Model.Dto;
+
+namespace MediaBrowser.WindowsPhone.Converters
+{
+    public class EpisodeNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            var item = value as BaseItemDto;
+            if (item == null)
+            {
+                return string.Empty;
+            }
+
+            if (item.IndexNumberEnd.HasValue)
+            {
+                return string.Format("{0}-{1}", item.IndexNumber, item.IndexNumberEnd);
+            }
+
+            return item.IndexNumber.HasValue ? item.IndexNumber.Value.ToString(CultureInfo.InvariantCulture) : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
