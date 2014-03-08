c3d9e29283b2816ebf653e86c45d3bea89e31ca2
ScottIsAFool <scottisafool@live.co.uk>
2014-03-08 15:04:22 +0000

    Fixes for now playing background enhancement

--- MediaBrowser.WindowsPhone8/Views/NowPlayingView.xaml.cs
+++ MediaBrowser.WindowsPhone8/Views/NowPlayingView.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+﻿using System.Windows.Navigation;
 using GalaSoft.MvvmLight.Messaging;
-using MediaBrowser.WindowsPhone.ViewModel;
 
 namespace MediaBrowser.WindowsPhone.Views
 {
@@ -18,23 +14,6 @@ namespace MediaBrowser.WindowsPhone.Views
         public NowPlayingView()
         {
             InitializeComponent();
-
-            Loaded += (sender, args) =>
-            {
-                var item = (DataContext as PlaylistViewModel).NowPlayingItem;
-
-                var url = item.BackgroundImageUrl;
-
-                if (!string.IsNullOrEmpty(url))
-                {
-                    GridForBackground.Background = new ImageBrush
-                    {
-                        Stretch = Stretch.UniformToFill,
-                        Opacity = 0.2,
-                        ImageSource = new BitmapImage(new Uri(url))
-                    };
-                }
-            };
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
