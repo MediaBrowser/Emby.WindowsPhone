b9b5cacd6f47c0aee30a49b8a75cf16385802559
Scott Lovegrove <scottisafool@live.co.uk>
2014-10-20 19:11:10 +0100

    Preparatory work for picture upload

---
+++ MediaBrowser.WindowsPhone.Background/ScheduledAgent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Cimbalino.Phone.Toolkit.Helpers;
+using Cimbalino.Phone.Toolkit.Services;
+using MediaBrowser.Model;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Session;
+using MediaBrowser.Services;
+using MediaBrowser.WindowsPhone.Model;
+using Microsoft.Phone.Scheduler;
+using ScottIsAFool.WindowsPhone.Logging;
+
+namespace MediaBrowser.WindowsPhone.Background
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        private readonly IExtendedApiClient _apiClient;
+        private static ILog _logger;
+
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        public ScheduledAgent()
+        {
+            _logger = new WPLogger(GetType());
+            _apiClient = CreateClient();
+            WPLogger.AppVersion = ApplicationManifest.Current.App.Version;
+            WPLogger.LogConfiguration.LogType = LogType.WriteToFile;
+            WPLogger.LogConfiguration.LoggingIsEnabled = true;
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        private static IExtendedApiClient CreateClient()
+        {
+            try
+            {
+                var applicationSettings = new ApplicationSettingsService();
+                var connectionDetails = applicationSettings.Get<ConnectionDetails>(Constants.Settings.ConnectionSettings);
+                var device = new Device { DeviceId = SharedUtils.GetDeviceId(), DeviceName = SharedUtils.GetDeviceName() };
+                var client = new ExtendedApiClient(new NullLogger(), connectionDetails.ServerAddress, "Windows Phone 8", device, ApplicationManifest.Current.App.Version, new ClientCapabilities{ SupportsContentUploading = true});
+
+                AuthenticationService.Current.Start(client);
+
+                return client;
+            }
+            catch (Exception ex)
+            {
+                _logger.FatalException("Error creating ApiClient", ex);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            var uploadTask = task as ResourceIntensiveTask;
+            if (uploadTask == null)
+            {
+                NotifyComplete();
+            }
+
+            NotifyComplete();
+        }
+    }
+}
--- MediaBrowser.WindowsPhone8/MediaBrowserUriMapper.cs
+++ MediaBrowser.WindowsPhone8/MediaBrowserUriMapper.cs
@@ -6,12 +6,18 @@ namespace MediaBrowser.WindowsPhone
     public class MediaBrowserUriMapper : UriMapperBase
     {
         private const string Protocol = "mediabrowser";
+        private const string PhotoUploadProtocol = "ConfigurePhotosUploadSettings";
 
         public override Uri MapUri(Uri uri)
         {
             if (uri.ToString().ToLower().Contains(Protocol))
             {
                 return new Uri(Constants.Pages.SplashScreen, UriKind.Relative);
+            }
+
+            if (uri.ToString().Contains(PhotoUploadProtocol))
+            {
+                
             }
 
             return uri;
