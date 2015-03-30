099a076c68220f5b006149fc90e5070b21219a0b
Scott Lovegrove <scottisafool@live.co.uk>
2015-03-30 16:25:41 +0100

    Changed device ID

--- MediaBrowser.Shared/SharedUtils.cs
+++ MediaBrowser.Shared/SharedUtils.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using Ailon.WP.Utils;
+using MediaBrowser.ApiInteraction.Cryptography;
 using MediaBrowser.WindowsPhone.CimbalinoToolkit;
+using MediaBrowser.WindowsPhone.Model.Security;
 using Microsoft.Phone.Info;
 
 namespace MediaBrowser.WindowsPhone
 {
     public static class SharedUtils
     {
+        private static ICryptographyProvider _crypt = new CryptographyProvider();
+
         public static void CopyItem<T>(this T source, T destination) where T : class
         {
             foreach (var sourcePropertyInfo in source.GetType().GetProperties())
@@ -28,7 +32,7 @@ namespace MediaBrowser.WindowsPhone
             var deviceName = DeviceStatus.DeviceName;
             var deviceId = DeviceStatus.DeviceManufacturer;
             var phone = PhoneNameResolver.Resolve(deviceId, deviceName);
-            var deviceInfo = String.Format("{0} ({1})", phone.CanonicalModel, phone.CanonicalManufacturer);
+            var deviceInfo = string.Format("{0} ({1})", phone.CanonicalModel, phone.CanonicalManufacturer);
 
             return deviceInfo;
         }
@@ -36,9 +40,9 @@ namespace MediaBrowser.WindowsPhone
         public static string GetDeviceId()
         {
             var uniqueId = new DeviceExtendedPropertiesService().DeviceUniqueId;
-            var deviceId = Convert.ToBase64String(uniqueId, 0, uniqueId.Length);
-
-            return deviceId;
+            var hashedId = _crypt.CreateMD5(uniqueId);
+            
+            return new Guid(hashedId).ToString();
         }
     }
 }
