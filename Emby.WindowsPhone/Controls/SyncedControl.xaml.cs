df54785ba35b91e87e51dff1c51f6673b19165d6
Scott Lovegrove <scottisafool@live.co.uk>
2015-07-23 19:12:53 +0100

    Add sync control

---
+++ Emby.WindowsPhone/Controls/SyncedControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Emby.WindowsPhone.Controls
+{
+    public partial class SyncedControl : UserControl
+    {
+        public SyncedControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
